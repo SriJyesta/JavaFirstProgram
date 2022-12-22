adab1496a777ed8788153a6cc1d4dc8827ef45cd
SriJyesta <srivjyesta@gmail.com>
2022-12-22 15:05:09 -0800

    first commit

---
+++ src/day1/FirstJavaProgram.java
@@ -0,0 +1,16 @@
+package day1;
+
+public class FirstJavaProgram 
+{
+	
+	public static void main(String args[])
+	{
+		//statements code webdriver
+		
+		System.out.print("New java project");
+		
+		System.out.print(10+10+10+10+10);
+		//run the code and see results in console
+	}
+
+}
---
+++ src/day1/SecondJavaProgram.java
@@ -0,0 +1,35 @@
+package day1;
+
+public class SecondJavaProgram {
+	
+	
+
+	public static void main(String[] args) {
+		// Write selenium here
+		System.out.println("New java project");
+		String name = "Automation";
+		System.out.println(name);
+		int a = 1;
+		int b = 2;
+
+		SecondJavaProgram.testing(name, a, b);
+	}
+
+	public static void testing(String name,int a, int b) {
+		// Write selenium here
+		System.out.println("New java project");
+		name = "Selenium";
+
+		System.out.println(name);
+		
+		
+		System.out.println(a+b);
+		
+		String d="Hi";
+		String g="Selenium";
+		
+		System.out.println(d  + g );
+
+
+	}
+}
