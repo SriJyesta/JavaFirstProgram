2f6429b74f3a1030cc79d89d29d5a19179ac13dd
SriJyesta <srivjyesta@gmail.com>
2022-12-28 10:55:34 -0800

    new code

---
+++ src/day1/TestingJavaCode.java
@@ -0,0 +1,27 @@
+package day1;
+
+public class TestingJavaCode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		char value = 'n';
+		int code = 789;
+		TestingJavaCode.test(value, code);
+
+	}
+
+	public static void test(char value, int code) {
+
+		System.out.println(value + code);
+		System.out.println(value);
+		System.out.println(code);
+
+	}
+
+	public static void test1() {
+		System.out.println("");
+		
+
+	}
+}
