0c59c03b82e51090911e961d75e24a87d9fab33f
SriJyesta <srivjyesta@gmail.com>
2022-12-29 21:00:06 -0800

    r test

--- src/day1/TestingJavaCode.java
+++ src/day1/TestingJavaCode.java
@@ -1,10 +1,10 @@
 package day1;
 
 public class TestingJavaCode {
-	
-	public static int a;
-	public static String b;
-	
+
+	public static int a = 1;
+	public static String b = "Jyesta";
+	public static String c = "SriVenkat";
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -12,8 +12,8 @@ public class TestingJavaCode {
 		int value = 1;
 		int code = 789;
 		int s = 89;
-		
-		double deci=10.23;
+
+		double deci = 10.23;
 		TestingJavaCode.test(value, code, s);
 		TestingJavaCode.test1(deci);
 
@@ -21,7 +21,7 @@ public class TestingJavaCode {
 
 	public static void test(int value, int code, int s) {
 
-		System.out.println(value+"  "+code+"   "+s);
+		System.out.println(value + "  " + code + "   " + s);
 		System.out.println(value);
 		System.out.println(code);
 
@@ -29,13 +29,9 @@ public class TestingJavaCode {
 
 	public static void test1(double deci) {
 		System.out.println(deci);
-		TestingJavaCode.a=345;
-		TestingJavaCode.b="Jyesta";
 		System.out.println(a);
 		System.out.println(b);
-		
-		
-		
+		System.out.println(c);
 
 	}
 }
--- src/day2/DataTypesVarDemo.java
+++ src/day2/DataTypesVarDemo.java
@@ -4,27 +4,25 @@ import day1.TestingJavaCode;
 
 public class DataTypesVarDemo {
 
-	public static void main(String[] args) 
-	
+	public static void main(String[] args)
+
 	{
 		// declaring var inside main method
 
 		int value = 56;
 		int code = 789;
 		int s;
-		s=2;
+		s = 2;
 		double deci = 10.23;
+		System.out.println(s);
 		TestingJavaCode.test(value, code, s);
 		TestingJavaCode.test1(deci);
-
-		System.out.println(s);
-		
 
 	}
 
 	public static void test(int value, int code, int s) {
 
-		System.out.println(value+"  "+code+"   "+s);
+		System.out.println(value + "  " + code + "   " + s);
 		System.out.println(value);
 		System.out.println(code);
 
